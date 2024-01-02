55edc64ad66053c8932bee841fad5125df6139ef
IM09112001 <83290696+IM09112001@users.noreply.github.com>
2024-01-02 13:24:41 +0500

    payment, checkout and store integration

--- store/urls.py
+++ store/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import StoreListView
+from .views import StoreListView, cart, store, checkout, updateItem, proccessOrder
 
 app_name = 'store'
 
 
 urlpatterns = [
     path("", StoreListView.as_view(), name='list'),
+    path('store/', store, name="store"),
+	path('cart/', cart, name="cart"),
+	path('checkout/', checkout, name="checkout"),
+    
+	path('update_item/', updateItem, name="update_item"),
+	path('process_order/', proccessOrder, name="process_order"),
 ]
+
--- store/views.py
+++ store/views.py
@@ -1,6 +1,105 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import JsonResponse
+import json
+import datetime
+
+from .models import *
+from .utils import cookieCart, cartData, guestOrder
 
 class StoreListView(TemplateView):
     template_name = 'cart.html'
 
+
+def store(request):
+    data = cartData(request)
+
+    cartItems = data['cartItems']          
+
+    products = Product.objects.all()
+    context = {
+        'products':products,
+        'cartItems': cartItems
+        }
+    return render(request, 'store.html', context=context)
+
+def cart(request):
+    data = cartData(request)
+
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+
+    context = {'items':items, 'order':order, 'cartItems': cartItems}
+    return render(request, 'cart.html', context)
+
+def checkout(request):
+    data = cartData(request)
+
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+
+    context = {'items':items, 'order':order, 'cartItems': cartItems}
+    return render(request, 'checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('Data:', data)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity += 1
+    elif action == 'remove':
+        orderItem.quantity -= 1
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+    
+    return JsonResponse('Item was added', safe=False)
+
+# from django.views.decorators.csrf import csrf_exempt
+
+# @csrf_exempt
+def proccessOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    else:
+        print('user is not logged in...')
+        print('COOKIES:', request.COOKIES)
+
+        customer, order = guestOrder(request=request, data=data)
+
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode']
+        )
+
+    order.save()
+
+    return JsonResponse('Payment submitted...', safe=False)
